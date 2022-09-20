e1ae2387ca8d1c134b2f64d0354d09c47a8e3bed
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-20 10:07:17 -0700

    Initialization Demonstration

--- src/main/java/com/tlglearning/animals/Dog.java
+++ src/main/java/com/tlglearning/animals/Dog.java
@@ -2,6 +2,41 @@ package com.tlglearning.animals;
 
 public class Dog extends Wolf {
 
+  private static final int MY_CONSTANT = initializeValue();
+
+  private int data = initializeData();
+
+  static {
+    System.out.println("Dog initialization block");
+  }
+
+
+  {
+    System.out.println("Dog instance initialization block");
+  }
+
+  {
+    System.out.println("second Dog initialization block");
+  }
+
+  public Dog() {
+    System.out.println("Dog constructor");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Dog.initializeValue");
+    return -2;
+  }
+
+  public static void whoAmI() {
+    System.out.println("I am a dog");
+  }
+
+  private int initializeData() {
+    System.out.println("Dog.initializeData");
+    return 2;
+  }
+
   @Override
   public void vocalize() {
     System.out.println("Bark like a dog!");
@@ -11,11 +46,9 @@ public class Dog extends Wolf {
   public void hunt() {
     System.out.println("Hunts for balls, bones, leaves, etc.");
   }
+
   public void rollOver() {
     System.out.println("I'm rolling over! I'm the best Doggo");
   }
-
-  public static void whoAmI() {
-    System.out.println("I am a dog");
-  }
 }
+
